cc1998a8d250e828b4edb14982a6a9df1842d07a
Cookie987 <bili.cookie987@outlook.com>
2025-11-10 17:17:50 +0800

    13
    Removed unused ESP32 variant imports from power management.

--- esphome/components/power_management/__init__.py
+++ esphome/components/power_management/__init__.py
@@ -1,11 +1,7 @@
 from esphome import automation
 import esphome.codegen as cg
 from esphome.components.esp32 import (
-    VARIANT_ESP32C6,
-    VARIANT_ESP32H2,
-    VARIANT_ESP32S3,
     add_idf_sdkconfig_option,
-    only_on_variant,
 )
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
